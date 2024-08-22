986ee629aee0629771176a98827d7184026bb7ff
bipan <bipan13345@gmail.com>
2024-08-22 13:18:15 +1000

    Tailwind css added.

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
+import { CompanySearch } from "../../company";
+import AddPortfolio from "../Portfolio/AddPortfolio/AddPortfolio";
 
 interface Props {
-    companyName : string;
-    ticker : string;
-    price : number;
+  id : string;
+  searchResult : CompanySearch;
+  onPortfolioCreate : (e : SyntheticEvent) => void;
 }
 
-const Card : React.FC<Props> = ({companyName, ticker, price}: Props) : JSX.Element => {
+const Card : React.FC<Props> = ({searchResult, id, onPortfolioCreate}: Props) : JSX.Element => {
   return (
-    <div className='flex flex-col items-center justify-between w-full p-6 bg-slate-100 rounded-lg md:flex-row'>
-        <img alt="Movie" src='fsdfsd'/>
-        <div className='details'>
-            <h2>{companyName} ({ticker})</h2>
-            <p>${price}</p>
-        </div>
-        <p className='info'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum deleniti minus nisi laborum ullam in dignissimos facere, 
-            voluptatum illum cumque voluptates obcaecati qui ad, omnis amet? Dolorem ad iure ea?</p>
+    <div
+        className="flex flex-col items-center justify-between w-full p-6 bg-slate-100 rounded-lg md:flex-row"
+        key={id}
+        id={id}
+    >
+      <h2 className="font-bold text-center text-black md:text-left">
+        {searchResult.name} ({searchResult.symbol})
+      </h2>
+      <p className="text-black">{searchResult.currency}</p>
+      <p className="font-bold text-black">
+        {searchResult.exchangeShortName} - {searchResult.currency}
+      </p>
+      <AddPortfolio onPortfolioCreate={onPortfolioCreate} symbol={searchResult.symbol}/>
     </div>
   )
 }
---
+++ src/Components/Portfolio/ListPotfolio/ListPortfolio.tsx
@@ -0,0 +1,39 @@
+import React, { SyntheticEvent } from 'react'
+import Cardportfolio from '../CardPortfolio/Cardportfolio'
+
+interface Props {
+    portfolioValues : string[];
+    onPortDelete : (e : SyntheticEvent) => void;
+}
+
+function ListPortfolio({portfolioValues, onPortDelete}: Props) {
+  return (
+    <>
+    <section id="portfolio">
+      <h2 className="mb-3 mt-3 text-3xl font-semibold text-center md:text-4xl">
+        My Portfolio
+      </h2>
+      <div className="relative flex flex-col items-center max-w-5xl mx-auto space-y-10 px-10 mb-5 md:px-6 md:space-y-0 md:space-x-7 md:flex-row">
+        <>
+          {portfolioValues.length > 0 ? (
+            portfolioValues.map((portfolioValue) => {
+              return (
+                <Cardportfolio
+                  portfolioValue={portfolioValue}
+                  onPortDelete={onPortDelete}
+                />
+              );
+            })
+          ) : (
+            <h3 className="mb-3 mt-3 text-xl font-semibold text-center md:text-xl">
+              Your portfolio is empty.
+            </h3>
+          )}
+        </>
+      </div>
+    </section>
+    </>
+  )
+}
+
+export default ListPortfolio
